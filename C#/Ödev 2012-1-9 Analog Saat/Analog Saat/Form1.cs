8ba7558f7940d0f92c9120d97799724d06cf348c
webkolog <webkolog@gmail.com>
2025-06-26 01:41:01 +0300

    Add files via upload

---
+++ C#/Ödev 2012-1-9 Analog Saat/Analog Saat/Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Analog_Saat
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            panel1.Refresh();
+            Pen MaviKalem = new Pen(Color.Blue, 10f);
+            Pen KirmiziKalem = new Pen(Color.Red, 8f);
+            Pen SariKalem = new Pen(Color.Yellow, 4f);
+            Graphics akrep = panel1.CreateGraphics();
+            Graphics yelkova = panel1.CreateGraphics();
+            Graphics saniye = panel1.CreateGraphics();
+            int i_saat = System.DateTime.Now.Hour;
+            int i_dak = DateTime.Now.Minute;
+            int i_san = DateTime.Now.Second;
+            int Yarisi = panel1.Width / 2;
+            akrep.TranslateTransform(Yarisi, Yarisi);
+            akrep.RotateTransform(Convert.ToInt32(Math.Round((double)i_saat / 12 * 360, 0)));
+            akrep.DrawLine(MaviKalem, 0, 0, 0, -60);
+            yelkova.TranslateTransform(180, 180);
+            yelkova.RotateTransform(Convert.ToInt32(Math.Round((double)i_dak / 60 * 360, 0)));
+            yelkova.DrawLine(KirmiziKalem, 0, 0, 0, -90);
+            saniye.TranslateTransform(180, 180);
+            saniye.RotateTransform(Convert.ToInt32(Math.Round((double)i_san / 60 * 360, 0)));
+            saniye.DrawLine(SariKalem, 0, 0, 0, -110);
+            
+        }
+    }
+}
---
+++ C#/Ödev 2012-2-10 Seçilen silineni geri alma/07-AdoOrnek/07-AdoOrnek/Form1.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace _07_AdoOrnek
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        DataTable dt;
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            SqlConnection cnn = new SqlConnection("server=.; Database=KuzeyRuzgari; trusted_connection=true");
+            SqlDataAdapter adp = new SqlDataAdapter("SELECT * FROM Urunler", cnn);
+            dt = new DataTable();
+            adp.Fill(dt);
+            dataGridView1.DataSource = dt;
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            DataView dv = new DataView(dt);
+            //Flan = 323 OR Filan= 'ABS'
+            try
+            {
+                dv.RowFilter = "UrunAdi LIKE '%" + textBox1.Text + "%' OR Fiyat = " + textBox1.Text;
+            }
+            catch
+            {
+                dv.RowFilter = "UrunAdi LIKE '%" + textBox1.Text + "%'";
+            }
+            //"UrunAdi LIKE 'gra%'"
+            dataGridView1.DataSource = dv;
+        }
+
+        private void btnSilinenleriGoster_Click(object sender, EventArgs e)
+        {
+            DataView dv = new DataView(dt);
+            dv.RowStateFilter = DataViewRowState.Deleted;
+            dataGridView2.DataSource = dv;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            foreach (DataGridViewRow dgvr in dataGridView2.SelectedRows)
+            {
+                int i = 0;
+                foreach (DataRow dc in dt.Rows)
+                {
+                    if (dc.RowState == DataRowState.Deleted)
+                    {
+                        if (dgvr.Index == i)
+                        {
+                            dc.RejectChanges();
+                        }
+                        i++;
+                    }
+                }
+            }
+        }
+    }
+}
